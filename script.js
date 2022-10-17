d728aec0c7277c3e72edf11a20208178466663e2
Shizuu <coolcoledude111@gmail.com>
2022-10-17 12:13:01 +0100

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let num1 = []
-let num2 = []
+let answer = ''
 let total = []
 let value = 0
 let choice = ""
@@ -12,8 +12,6 @@ const removeBtn = document.querySelector('.delete')
 const numberBtn = document.querySelectorAll("#num")
 const operator = document.querySelectorAll("#operator")
 
-
-
 numberBtn.forEach(button =>{
     button.addEventListener('click' , () =>{
         let value = button.getAttribute('value')
@@ -22,103 +20,96 @@ numberBtn.forEach(button =>{
     })
 })
 
-
 operator.forEach(button =>{
     button.addEventListener('click', () =>{
         let choice = button.getAttribute('value')
         if (choice == "add"){
             addition()
-            console.log(total)
         }else if (choice == "subtract"){
             subtraction()
-            console.log(total)
         }else if (choice == "multiply"){
             multiplication()
-            console.log(total)
         }else if (choice == "divide"){
             division()
-            console.log(total)
         }else if (choice == "equal"){
             equal()
-            console.log(total)
         }
     })
 })
-
 
 
 clearBtn.addEventListener('click', () =>{
     result.textContent = 0
     total = []
     num1 = []
-    num2 = []
+    answer = []
     choice = ""
 })
 
+//fix this button to remove number
 removeBtn.addEventListener('click', () =>{
     num1.pop()
     result.textContent = num1.join("")
- 
 })
 
 
+function checkNumber(){
+    if (typeof answer == 'number'){
+        total = answer;
+    }else{
+        total = parseInt(num1.join(""))
+    }
+    num1 = []
+}
 
 
 function addition(){
-    total = parseInt(num1.join(""))
-    console.log(num1)
-    console.log(total)
-    num1 = []
+    checkNumber()
     operation = "add"
     
 }
 
 function subtraction(){
-    total = parseInt(num1.join(""))
-    console.log(num1)
-    console.log(total)
-    num1 = []
+    checkNumber()
     operation = "subtract"
+    
 }
 
 function multiplication(){
-    total = parseInt(num1.join(""))
-    console.log(num1)
-    console.log(total)
-    num1 = []
+    checkNumber()
     operation = "multiply"
     
 }
 
 function division(){
-    total = parseInt(num1.join(""))
-    console.log(num1)
-    console.log(total)
-    num1 = []
+    checkNumber()
     operation = "divide"
     
 }
 
-
-
-
 function equal(){
     if (operation == "add"){
-        total += parseInt(num1.join(""))
-        result.textContent = total
+        answer = total + parseInt(num1.join(""))
+        result.textContent = answer
+        
     }
     else if (operation == "subtract"){
-        total -= parseInt(num1.join(""))
-        result.textContent = total
+        answer = total
+        answer -= parseInt(num1.join(""))
+        result.textContent = answer
     }
     else if (operation == "multiply"){
-        total *= parseInt(num1.join(""))
-        result.textContent = total
+        answer = total
+        answer *= parseInt(num1.join(""))
+        result.textContent = answer
     }
     else if (operation == "divide"){
-        total /= parseInt(num1.join(""))
-        result.textContent = total
+        answer = total
+        answer /= parseInt(num1.join(""))
+        result.textContent = answer
     } 
 }
 
 //store value in a different variable and put value after equal in a different one as well?
+//if num2 === number;
+//run the function operator
